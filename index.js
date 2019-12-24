dfaf17da4d25df66485a3a931409741490dfaeb2
Vincent Cuenca <vincent.e.cuenca@gmail.com>
2019-12-24 14:28:21 -0500

    a

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
 'use strict';
+const { ReportBase } = require('istanbul-lib-report');
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function AwsCloudwatchMetricsReport(opts) {
-  this.file = opts.file || 'metric_data.json';
-}
-
-AwsCloudwatchMetricsReport.prototype.onStart = function (root, context) {
-  this.contentWriter = context.writer.writeFile(this.file);
-  var summary = root.getCoverageSummary();
-  var metrics = [];
-  var timestamp = new Date();
-
-  for (var dimension in summary.data) {
-    for (var metricType in summary.data[dimension]) {
-      var metric = {
-        MetricName: capitalizeFirstLetter(metricType),
-        Dimensions: [
-          {
-            Name: 'Coverage',
-            Value: capitalizeFirstLetter(dimension),
-          },
-        ],
-        Timestamp: timestamp,
-        Unit: metricType === 'pct' ? 'Percent' : 'Count',
-        Value: summary[dimension][metricType],
-      };
-      metrics.push(metric);
-    }
+class AwsCloudwatchMetricsReport extends ReportBase {
+  constructor(opts) {
+    super();
+    this.file = opts.file || 'metric_data.json';
   }
 
-  this.contentWriter.write(JSON.stringify(metrics));
-  this.contentWriter.close();
-};
+  onStart(root, context) {
+    this.contentWriter = context.writer.writeFile(this.file);
+    var summary = root.getCoverageSummary();
+    var metrics = [];
+    var timestamp = new Date();
+
+    for (var dimension in summary.data) {
+      for (var metricType in summary.data[dimension]) {
+        var metric = {
+          MetricName: capitalizeFirstLetter(metricType),
+          Dimensions: [
+            {
+              Name: 'Coverage',
+              Value: capitalizeFirstLetter(dimension),
+            },
+          ],
+          Timestamp: timestamp,
+          Unit: metricType === 'pct' ? 'Percent' : 'Count',
+          Value: summary[dimension][metricType],
+        };
+        metrics.push(metric);
+      }
+    }
+
+    this.contentWriter.write(JSON.stringify(metrics));
+    this.contentWriter.close();
+  };
+}
 
 module.exports = AwsCloudwatchMetricsReport;
